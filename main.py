3fb0ae0172c9d05c1f18a15b28326c5323ecd488
krishna-allam <krishna.allam13@gmail.com>
2023-11-08 20:06:43 -0600

    Created a read_text_file function to encapsulate file reading. Modified the letter_counter function to check if a character is alphabetic and use the get method for cleaner dictionary population. Combined the functions for printing letter counts into a single function, print_letter_counts. Moved the main function to the end and added a conditional check for __name__ to ensure it's only executed when the script is run directly.

--- main.py
+++ main.py
@@ -1,36 +1,34 @@
-TEXT_PATH = "books/frankenstein.txt"
-with open(TEXT_PATH) as file:
-    file_contents = file.read()
+def read_text_file(file_path):
+    with open(file_path) as file:
+        return file.read()
 
-def word_counter(file_contents):
-    return len(file_contents.split())
+def word_counter(text):
+    return len(text.split())
 
-def letter_counter(file_contents):
+def letter_counter(text):
     letter_count_dict = {}
-    for letter in file_contents:
-        if letter.lower() in letter_count_dict:
-            letter_count_dict[letter.lower()] += 1
-        else:
-            letter_count_dict[letter.lower()] = 1
+    for letter in text:
+        letter = letter.lower()
+        if letter.isalpha():
+            letter_count_dict[letter] = letter_count_dict.get(letter, 0) + 1
     return letter_count_dict
 
-def letter_count_logger(sorted_dict):
-    for letter, count in sorted_dict:
-        if letter.isalpha():
-            print(f"The {letter} character was found {count} times")
-
-def word_count_logger(word_count):
-    print(f"{word_count} words found in the document")
-
-def sort_dict_values(dictionary):
-    return sorted(dictionary.items(), key=lambda x:x[1])
+def print_letter_counts(letter_count_dict):
+    for letter, count in sorted(letter_count_dict.items()):
+        print(f"The {letter} character was found {count} times")
 
 def main():
+    TEXT_PATH = "books/frankenstein.txt"
+    file_contents = read_text_file(TEXT_PATH)
+
     print("--- Begin report of books/frankenstein.txt ---")
-    word_count_logger(word_counter(file_contents))
-    print("\n")
-    letter_count_logger(sort_dict_values(letter_counter(file_contents)))
+    word_count = word_counter(file_contents)
+    print(f"{word_count} words found in the document\n")
+    
+    letter_count_dict = letter_counter(file_contents)
+    print_letter_counts(letter_count_dict)
+    
     print("--- End report ---")
 
-main()
-
+if __name__ == "__main__":
+    main()
